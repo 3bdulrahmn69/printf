ac020de010830e0cf784f031a2cf93797f2c5407
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-04-26 17:08:17 +0200

    test

--- _int_to_bin.c
+++ _int_to_bin.c
@@ -8,47 +8,47 @@
  */
 char *zero(void)
 {
-char *res;
+    char *res;
 
-res = malloc(2);
-if (!res)
-return (NULL);
-res[0] = '0';
-res[1] = '\0';
-return (res);
+    res = malloc(2);
+    if (!res)
+        return (NULL);
+    res[0] = '0';
+    res[1] = '\0';
+    return (res);
 }
 
 /**
-* intToBinary - fjsd
-* @num: sodpjga
-* Return: char *
-*/
+ * intToBinary - fjsd
+ * @num: sodpjga
+ * Return: char *
+ */
 char *intToBinary(unsigned int num)
 {
-unsigned int i = 1, digits = 1, j;
-char *res;
+    unsigned int i = 1, digits = 1, j;
+    char *res;
 
-if (num == 0)
-{
-res = zero();
-return (res);
-}
-while (i * 2 < num)
-{
-i *= 2;
-digits++;
-}
-res = malloc(digits + 1);
-if (!res)
-return (NULL);
+    if (num == 0)
+    {
+        res = zero();
+        return (res);
+    }
+    while (i * 2 <= num)
+    {
+        i *= 2;
+        digits++;
+    }
+    res = malloc(digits + 1);
+    if (!res)
+        return (NULL);
 
-i = num;
-j = digits - 1;
-while (i > 0)
-{
-res[j--] = (i % 2) + '0';
-i /= 2;
-}
-res[digits] = '\0';
-return (res);
+    i = num;
+    j = digits - 1;
+    while (i > 0)
+    {
+        res[j--] = (i % 2) + '0';
+        i /= 2;
+    }
+    res[digits] = '\0';
+    return (res);
 }
