8f0a800a456300b1496a15ebdbe1b52140bee537
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-04-29 02:22:32 +0300

    test

--- _printf_ptr.c
+++ _printf_ptr.c
@@ -7,28 +7,30 @@
  */
 int _printf_ptr(va_list args)
 {
-    intptr_t num = (intptr_t)va_arg(args, void *);
-    int count = 0, i;
-    int digits[32];
+intptr_t num = (intptr_t)va_arg(args, void *);
+int count = 0, i;
+int digits[32];
 
-    /* Compute the hexadecimal digits in the correct order */
-    int num_digits = 0;
+/* Compute the hexadecimal digits in the correct order */
+int num_digits = 0;
 
-    do
-    {
-        int digit = num % 16;
+if (!num)
+return (printStr("(nil)"));
 
-        digits[num_digits++] = digit < 10 ? digit + '0' : digit - 10 + 'a';
-        num /= 16;
-    } while (num > 0);
+do {
+int digit = num % 16;
 
-    count += printStr("0x");
+digits[num_digits++] = digit < 10 ? digit + '0' : digit - 10 + 'a';
+num /= 16;
+} while (num > 0);
 
-    /* Print the hexadecimal digits in the correct order */
-    for (i = num_digits - 1; i >= 0; i--)
-    {
-        count += _putchar(digits[i]);
-    }
+count += printStr("0x");
 
-    return (count);
+/* Print the hexadecimal digits in the correct order */
+for (i = num_digits - 1; i >= 0; i--)
+{
+count += _putchar(digits[i]);
+}
+
+return (count);
 }
